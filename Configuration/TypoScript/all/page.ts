9100fd4dec6b8761684312e3376d5da7dbe763be
phasenraum2010 <thomas@woehlke.org>
2017-04-14 19:59:36 +0200

    work in progress

--- Configuration/TypoScript/all/page.ts
+++ Configuration/TypoScript/all/page.ts
@@ -1,12 +1,17 @@
 page {
 	includeCSS {
-		file1 = https://fonts.googleapis.com/css?family=Titillium+Web
-		file2 = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/css/map-icons.min.css
-		file3 = EXT:oekumene_tiergarten/Resources/Public/css/my.css
+		cssFileGoogleFontTitilliumWeb = https://fonts.googleapis.com/css?family=Titillium+Web
+		cssFileMapIcons = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/css/map-icons.min.css
+		cssFileMyOwn = EXT:oekumene_tiergarten/Resources/Public/css/my.css
 	}
 	includeJS {
-		file1 = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/js/map-icons.min.js
-		file2 = EXT:oekumene_tiergarten/Resources/Public/js/my.js
+		jsFileWecMapInfoBubble = EXT:oekumene_tiergarten/Resources/Public/Contrib/wec-map/infobubble.js
+		jsFileWecMapNarkerManager = EXT:oekumene_tiergarten/Resources/Public/Contrib/wec-map/markermanager.js
+		jsFileWecMapOms = EXT:oekumene_tiergarten/Resources/Public/Contrib/wec-map/oms.min.js
+		jsFileWecMapCopyrights = EXT:wec_map/Resources/Public/JavaScript/copyrights.js
+		jsFileWecMapWecMap = EXT:wec_map/Resources/Public/JavaScript/wecmap.js
+		jsFileMapIcons = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/js/map-icons.min.js
+		jsFileMyOwn = EXT:oekumene_tiergarten/Resources/Public/js/my.js
 	}
 	10 {
 		templateRootPaths {
@@ -28,7 +33,7 @@ page.config {
 	headerComment (
 --------------------------------------------------
 |                                                |
-|  TYPO3 Website - Development by               |
+|  TYPO3 Website - Development by                |
 |                                                |
 |  Thomas Woehlke | 2017 | www.thomas-woehlke.de |
 |                                                |
--- Configuration/TypoScript/all/plugin.ts
+++ Configuration/TypoScript/all/plugin.ts
@@ -71,7 +71,6 @@ plugin {
 	}
 	tx_indexedsearch {
 		templateFile = {$plugin.tx_indexedsearch.templateFile}
-
 	}
 	/*
 	tx_cal_controller {
@@ -327,3 +326,318 @@ plugin {
 }
 
 
+
+/*
+
+plugin {
+tx_cal_controller {
+subscribeFeUser = 1
+#activateFluid = 1
+subscribeWithCaptcha = 1
+dateConfig {
+splitSymbol = .
+dayPosition = 0
+yearPosition = 2
+}
+view {
+allowedViews = month,day,week,year,list,event,search_all,search_event,search_location,search_organizer,organizer,location,admin,create_event,confirm_event,save_event,edit_event,delete_event,remove_event,create_location,confirm_location,save_location,edit_loaction,delete_location,remove_location,create_organizer,confirm_organizer,save_organizer,edit_organizer,delete_organizer,remove_organizer
+
+# @description  Content object, that defines how a link is rendered by default
+defaultLinkSetup = TEXT
+defaultLinkSetup {
+current = 1
+typolink {
+parameter.field = link_parameter
+ATagParams.field = link_ATagParams
+additionalParams.field = link_additionalParams
+#section.field = link_section
+title.current = 1
+title.htmlSpecialChars = 1
+title.override {
+field = title
+required = 1
+}
+no_cache.field = link_no_cache
+useCacheHash = 1
+}
+}
+
+# @description  Content object, that defines how a link to a different view type is rendered by default
+defaultViewLinkSetup < .defaultLinkSetup
+defaultViewLinkSetup {
+typolink.title.override.override.cObject = TEXT
+typolink.title.override.override.cObject {
+field = view
+wrap = %%%|_VIEW%%%
+case = upper
+required = 1
+}
+}
+
+# @description  The content object used for back links
+backLink = TEXT
+backLink {
+value = <a href="javascript:history.back()">%%%BACK%%%</a>
+}
+
+# @description  Displays only events a current user is allowed to edit or delete
+showEditableEventsOnly = 0
+
+event {
+eventViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+createEventViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+editEventViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+deleteEventViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+event {
+defaultEventLength = 3600
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+day {
+dayViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+week {
+weekViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+month {
+monthViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+showListInMonthView = 1
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+year {
+yearViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+location {
+locationViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+createLocationViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+editLocationViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+deleteLocationViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+}
+organizer {
+organizerViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+createOrganizerViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+editOrganizerViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+deleteOrganizerViewPid = {$plugin.tx_oekumene_tiergarten.settings.calenderPluginPageId}
+}
+list {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+subscription {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+ics {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+single_ics {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+freeAndBusy {
+enable = 0
+}
+search_event {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+create_event {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+edit_event {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+confirm_event {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+}
+lib {
+list {
+event {
+attachment =< tt_content.uploads
+attachment {
+dataProcessing.10.references >
+dataProcessing.10.references {
+table = tx_cal_event
+uid.data = field:uid
+fieldName = attachment
+}
+}
+}
+}
+}
+}
+}
+
+plugin.tx_cal_controller.view.other.showLogin = 1
+plugin.tx_cal_controller.view.other.loginPageId = 26
+plugin.tx_cal_controller.view.other.userFolderId = 28
+plugin.tx_cal_controller.view.other.showSearch = 1
+plugin.tx_cal_controller.view.other.showGoto = 1
+plugin.tx_cal_controller.view.other.showJumps = 1
+plugin.tx_cal_controller.view.delete_location.locationViewPid = 25
+plugin.tx_cal_controller.view.delete_location.createLocationViewPid = 25
+plugin.tx_cal_controller.view.delete_location.editLocationViewPid = 25
+plugin.tx_cal_controller.view.delete_location.deleteLocationViewPid = 25
+plugin.tx_cal_controller.view.delete_organizer.locationViewPid = 25
+plugin.tx_cal_controller.view.delete_organizer.createLocationViewPid = 25
+plugin.tx_cal_controller.view.delete_organizer.editLocationViewPid = 25
+plugin.tx_cal_controller.view.delete_organizer.deleteLocationViewPid = 25
+plugin.tx_cal_controller.rights.create.event.notifyUsersOnPublicCreate = 1
+plugin.tx_cal_controller.rights.create.event.publicEvents.group = 1
+plugin.tx_cal_controller.rights.create.organizer.group = 1
+plugin.tx_cal_controller.rights.create.location.group = 1
+plugin.tx_cal_controller.rights.create.exceptionEvent.group = 1
+plugin.tx_cal_controller.rights.create.event.forTodayAndFuture.group = 1
+plugin.tx_cal_controller.rights.create.event.inPast.group = 1
+plugin.tx_cal_controller.rights.admin.user = 1
+plugin.tx_cal_controller.rights.allowedGroups = 1
+plugin.tx_cal_controller.rights.create.event.group = 1
+plugin.tx_cal_controller.rights.create.organizer.addFeGroupToShared = 1
+plugin.tx_cal_controller.rights.create.location.addFeGroupToShared = 1
+plugin.tx_cal_controller.rights.create.location.addFeUserToShared = 1
+plugin.tx_cal_controller.rights.create.event.addFeGroupToShared = 1
+plugin.tx_cal_controller.rights.create.event.addFeUserToNotify = 1
+plugin.tx_cal_controller.rights.edit.event.group = 1
+plugin.tx_cal_controller.rights.edit.event.addFeGroupToShared = 1
+plugin.tx_cal_controller.rights.edit.event.startedEvents.group = 1
+plugin.tx_cal_controller.rights.delete.organizer.user = 1
+plugin.tx_cal_controller.rights.delete.location.user = 1
+plugin.tx_cal_controller.rights.delete.event.group = 1
+plugin.tx_cal_controller.rights.delete.exceptionEvent.group = 1
+plugin.tx_cal_controller.calendarName = Termine der Ökumene in Berlin Tiergarten
+plugin.tx_cal_controller.subscribeFeUser = 1
+plugin.tx_cal_controller.rights.edit.event.public = 1
+plugin.tx_cal_controller.useNewTemplatesAndRendering = 1
+plugin.tx_cal_controller.pidList = 28
+plugin.tx_cal_controller.view.defaultLinkSetup.typolink.title.section.field = link_section
+
+
+*/
