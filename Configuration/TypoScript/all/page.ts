959d7f95fd7891e05754c9f2c7aa49b827c57e8a
phasenraum2010 <thomas@woehlke.org>
2017-04-16 13:59:50 +0200

    mobile navigation

--- Configuration/TypoScript/all/page.ts
+++ Configuration/TypoScript/all/page.ts
@@ -2,10 +2,14 @@ page {
 	includeCSS {
 		cssFileGoogleFontTitilliumWeb = https://fonts.googleapis.com/css?family=Titillium+Web
 		cssFileMapIcons = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/css/map-icons.min.css
+		mmenu1 = EXT:oekumene_tiergarten/Resources/Public/Contrib/mmenu/css/jquery.mmenu.all.css
+		mmenu2 = EXT:oekumene_tiergarten/Resources/Public/Contrib/mmenu/wrappers/bootstrap/jquery.mmenu.bootstrap.css
 		cssFileMyOwn = EXT:oekumene_tiergarten/Resources/Public/css/my.css
 	}
 	includeJSFooter {
 		jsFileMapIcons = EXT:oekumene_tiergarten/Resources/Public/Contrib/map-icons/js/map-icons.min.js
+		mmenu1 = EXT:oekumene_tiergarten/Resources/Public/Contrib/mmenu/js/jquery.mmenu.all.min.js
+		mmenu2 = EXT:oekumene_tiergarten/Resources/Public/Contrib/mmenu/wrappers/bootstrap/jquery.mmenu.bootstrap.min.js
 		jsFileMyOwn = EXT:oekumene_tiergarten/Resources/Public/js/my.js
 	}
 	10 {
@@ -38,3 +42,67 @@ page.config {
 	# Wenn gesetzt, werden externe Medien, auf die auf Seiten verlinkt wird, ebenfalls indiziert.
 	index_externals = 1
 }
+
+
+# mmenu main/left config
+page.jsFooterInline.5.26.value (
+ if(winWidth < 768 && $menuLeft.length) {
+ $('#menu-left').mmenu({
+ 'extensions': [
+ 'pagedim-black',
+ 'theme-dark',
+ 'effect-slide-menu',
+ 'shadow-page',
+ 'shadow-panels'
+ ],
+ 'keyboardNavigation': true,
+ 'screenReader': true,
+ 'counters': false,
+ 'navbar': {
+	'title': 'Oekumene Tiergarten'
+  },
+ 'navbars': [{
+	'position': 'top',
+	'content': [
+		'prev',
+		'title',
+		'close'
+	]
+  },{
+ 'position': 'bottom',
+ 'content': [
+ '<a class="glyphicon glyphicon-search" href="/information/suche/"></a>',
+ '<a class="fa fa-envelope" href="/kontakt/"></a>',
+ '<a class="fa fa-twitter" href="https://twitter.com/OekumeneMoabit\" target="_blank"></a>',
+ '<a class="fa fa-facebook-official" href="https://www.facebook.com/Ökumene-in-Berlin-Tiergarten-107187572947095/" target="_blank"></a>'
+ ]
+ }]
+ });
+ }
+)
+
+# mmenu right config
+page.jsFooterInline.5.27.value (
+ if(winWidth < 768 && $menuRight.length) {
+ $('#lml')
+ .after('<a id=\'rml\' href=\'#menu-right\'><i class=\'fa fa-chevron-circle-down fa-lg pull-right navbar-toggle\' aria-hidden=\'true\'></i></a>');
+ $('#menu-right').mmenu({
+ 'offCanvas': {
+ 'position': 'right'
+ },
+ 'extensions': [
+ 'pagedim-black',
+ 'theme-dark',
+ 'effect-slide-menu',
+ 'shadow-page',
+ 'shadow-panels'
+ ],
+ 'keyboardNavigation': true,
+ 'screenReader': true,
+ 'counters': false,
+ 'navbar': {
+	'title': 'Oekumene Tiergarten'
+  },
+ });
+ }
+)
--- Resources/Public/js/my.js
+++ Resources/Public/js/my.js
@@ -17,4 +17,3 @@ window[disableStr] = true;
 
 
 
-
