3e27d0c9de42a81c0f7798ad10cab98655e8a64f
phasenraum2010 <thomas@woehlke.org>
2017-04-16 05:12:47 +0200

    work on cal templates

--- Configuration/TypoScript/all/plugin.ts
+++ Configuration/TypoScript/all/plugin.ts
@@ -644,3 +644,85 @@ plugin.tx_cal_controller.view.defaultLinkSetup.typolink.title.section.field = li
 
 
 */
+
+
+plugin.tx_cal_controller.allowSubscribe = 0
+plugin.tx_cal_controller.activateFluid = 0
+plugin.tx_cal_controller.view.event.event.title.dataWrap = <h2>%%%TITLE_LABEL%%%:&nbsp; |</h2>
+plugin.tx_cal_controller.view.event.event.title.dataWrap = <h2>|</h2>
+plugin.tx_cal_controller.view.event.event.alldayTitle.dataWrap = <h2>|</h2>
+plugin.tx_cal_controller.view.event.event.startdate.dataWrap = <div class="tx-kirche-cal-event-datetime"><span class="tx-kirche-cal-event-startdate">|</span></div>
+plugin.tx_cal_controller.view.event.event.enddate.dataWrap = <span class="tx-kirche-cal-event-enddate">|</span>
+plugin.tx_cal_controller.view.event.event.description.dataWrap = <div class="tx-kirche-cal-event-description">|</div>
+plugin.tx_cal_controller.view.event.event.image.stdWrap.dataWrap = <div class="tx-kirche-cal-event-image">|</div>
+plugin.tx_cal_controller.view.event.event.starttime.dataWrap = <div class="tx-kirche-cal-event-starttime">|</div>
+plugin.tx_cal_controller.view.event.event.startdate.dataWrap = <span class="tx-kirche-cal-event-startdate">|</span>
+plugin.tx_cal_controller.view.event.event.endtime.dataWrap = <span class="tx-kirche-cal-event-endtime">|</span>
+plugin.tx_cal_controller.view.event.event.starttime.dataWrap = <span class="tx-kirche-cal-event-starttime">|</span>
+plugin.tx_cal_controller.view.event.event.category.dataWrap = <span class="tx-kirche-cal-event-category">|</div>
+plugin.tx_cal_controller.view.event.event.category.dataWrap = <span class="tx-kirche-cal-event-category">|</span>
+plugin.tx_cal_controller.view.event.event.category.dataWrap = <span class="tx-kirche-cal-event-category">|</span>
+plugin.tx_cal_controller.view.event.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.event.event.organizer.dataWrap = <span class="cal-event-organizer">|</span>
+plugin.tx_cal_controller.view.event.event.location.dataWrap = <span class="location">|</span>
+plugin.tx_cal_controller.view.event.event.location.dataWrap = <span class="cal-event-location"><span class="map-icon map-icon-postal-code"></span>|</span>
+plugin.tx_cal_controller.view.event.event.organizer.dataWrap = <span class="cal-event-organizer"><i class="fa fa-user-circle" aria-hidden="true"></i>|</span>
+plugin.tx_cal_controller.view.location.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.organizer.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.organizer.organizer.name.dataWrap = <span class="tx-kirche-cal-organizer-name"></span>
+plugin.tx_cal_controller.view.organizer.organizer.name.dataWrap = <span class="tx-kirche-cal-organizer-name">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.street.dataWrap = <span class="tx-kirche-cal-organizer-street">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.city.dataWrap = <span class="tx-kirche-cal-organizer-city">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.zip.dataWrap = <span class="tx-kirche-cal-organizer-zip">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.phone.dataWrap = <span class="tx-kirche-cal-organizer-phone">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.fax.dataWrap = <span class="tx-kirche-cal-organizer-fax">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.country.dataWrap = <span class="tx-kirche-cal--organizer-country">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.country.dataWrap = <span class="tx-kirche-cal-organizer-country">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.description.dataWrap = <span class="tx-kirche-cal-organizer.description">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.email.dataWrap = <span class="tx-kirche-cal-organizer-email">|</span>
+plugin.tx_cal_controller.view.organizer.organizer.link.dataWrap = <span class="tx-kirche-cal-organizer-link">|</span>
+plugin.tx_cal_controller.view.location.location.link.dataWrap = <div class="tx-kirche-cal-location-link">|</span>
+plugin.tx_cal_controller.view.location.location.email.dataWrap = <div class="tx-kirche-cal-location-email">|</div>
+plugin.tx_cal_controller.view.location.location.email.dataWrap = <span class="tx-kirche-cal-location-email">|</span>
+plugin.tx_cal_controller.view.location.location.description.dataWrap = <span class="tx-kirche-cal-location-description">|</span>
+plugin.tx_cal_controller.view.location.location.country.dataWrap = <span class="tx-kirche-cal-location-country">|</span>
+plugin.tx_cal_controller.view.location.location.fax.dataWrap = <span class="tx-kirche-cal-location-fax">|</span>
+plugin.tx_cal_controller.view.location.location.phone.dataWrap = <span class="tx-kirche-cal-location-phone">|</span>
+plugin.tx_cal_controller.view.location.location.zip.dataWrap = <span class="tx-kirche-cal-location-zip">|</span>
+plugin.tx_cal_controller.view.location.location.city.dataWrap = <span class="tx-kirche-cal-location-city">|</span>
+plugin.tx_cal_controller.view.backLink.value = <a href="javascript:history.back()"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i>%%%BACK%%%</a>
+plugin.tx_cal_controller.view.location.location.street.dataWrap = <span class="tx-kirche-cal-location-street">|</span>
+plugin.tx_cal_controller.view.location.location.name.dataWrap = <span class="tx-kirche-cal-location-name">|</span>
+plugin.tx_cal_controller.view.location.location.link.dataWrap = <span class="tx-kirche-cal-location-link">|</span>
+plugin.tx_cal_controller.view.location.location.phone.dataWrap = <div><i class="fa fa-phone" aria-hidden="true"></i><span class="tx-kirche-cal-location-phone">|</span></div>
+plugin.tx_cal_controller.view.location.location.email.dataWrap = <div><i class="tx-kirche-icon-email fa fa-envelope"></i><span class="tx-kirche-cal-location-email">|</span></div>
+plugin.tx_cal_controller.view.location.location.link.dataWrap = <div><span class="glyphicon glyphicon-link" aria-hidden="true"></span><span class="tx-kirche-cal-location-link">|</span></div>
+plugin.tx_cal_controller.view.location.location.fax.dataWrap = <div><i class="fa fa-fax" aria-hidden="true"></i><span class="tx-kirche-cal-location-fax">|</span></div>
+plugin.tx_cal_controller.view.location.location.description.dataWrap = <div class="tx-kirche-cal-description"><span class="tx-kirche-cal-location-description">|</span></div>
+plugin.tx_cal_controller.view.organizer.organizer.phone.dataWrap = <div><i class="fa fa-phone" aria-hidden="true"></i><span class="tx-kirche-cal-organizer-phone">|</span></div>
+plugin.tx_cal_controller.view.organizer.organizer.link.dataWrap = <div><span class="glyphicon glyphicon-link" aria-hidden="true"></span><span class="tx-kirche-cal-organizer-link">|</span></div>
+plugin.tx_cal_controller.view.organizer.organizer.email.dataWrap = <div><i class="tx-kirche-icon-email fa fa-envelope"></i><span class="tx-kirche-cal-organizer-email">|</span></div>
+plugin.tx_cal_controller.view.organizer.organizer.fax.dataWrap = <div><i class="fa fa-fax" aria-hidden="true"></i><span class="tx-kirche-cal-organizer-fax">|</span></div>
+plugin.tx_cal_controller.view.organizer.organizer.description.dataWrap = <div class="tx-kirche-cal-description"><span class="tx-kirche-cal-organizer.description">|</span></div>
+
+
+plugin.tx_cal_controller.view.day.displayDate.strftime = %d.%m.%Y
+plugin.tx_cal_controller.view.week.displayDate.1.strftime = %d.%m.
+plugin.tx_cal_controller.view.week.displayDate.2.strftime = %d.%m.%Y
+plugin.tx_cal_controller.view.day.displayDate.strftime = %d.%m.%Y
+plugin.tx_cal_controller.view.day.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.week.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.week.dateFormatWeek = %d.%m.%Y
+plugin.tx_cal_controller.view.event.notify.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.event.remind.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.year.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.event.todo.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.list.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.title.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.notification.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.todo.todo.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.todo.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.month.heading.1.strftime = %d.%m.%Y
+plugin.tx_cal_controller.lib.list.event.dateFormat = %d.%m.%Y
+plugin.tx_cal_controller.view.week.dateFormatWeekList = %a<br/>%d.%m.
+plugin.tx_cal_controller.view.week.dayViewLink.strftime = %a %d.%m.
--- Resources/Public/js/my.js
+++ Resources/Public/js/my.js
@@ -1,5 +1,5 @@
 /* my own CSS. (C) 2017 Der Oekumenische Arbeitskreis Tiergarten */
-/* Google analytics */
+/* Opt-Out Google analytics */
 
 var gaProperty = 'UA-96623703-1';
 var disableStr = 'ga-disable-' + gaProperty;
@@ -11,36 +11,9 @@ document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; pa
 window[disableStr] = true;
 }
 
-/* Google analytics */
-/*
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-93860809-1', 'auto');
-  ga('set', 'anonymizeIp', true);
-  ga('send', 'pageview');
-*/
-/* Google analytics */
 
 
 
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-/*  Piwik */
-/*
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//piwik.thomas-woehlke.de/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-*/
-/*  Piwik */
 
 
 
