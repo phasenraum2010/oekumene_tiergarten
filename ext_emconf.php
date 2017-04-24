5043d61a63403d842cfd2c0abae86dd598ed8053
phasenraum2010 <thomas@woehlke.org>
2017-04-24 08:10:02 +0200

    work in progress #18 #24 and #26

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '7.6.0-7.6.99',
-            'php' => '5.4.0-7.0.999',
+            'php' => '7.0.0-7.0.999',
             /* System-Extensions: */
             'info' => '7.6.0-7.6.99',
             'fluid' => '7.6.0-7.6.99',
